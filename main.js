f2cd83522a11b496910e8369ea025822af8dadb8
zedd45 <christopher.keen@gmail.com>
2016-12-13 01:17:28 -0500

    remove router logic; once built this doesn't work.

--- main.js
+++ main.js
@@ -13,38 +13,14 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FastClick from 'fastclick';
 import { Provider } from 'react-redux';
 
 import store from './core/store';
-import router from './core/router';
-import history from './core/history';
+import Homepage from './pages/home';
 
-let routes = require('./routes.json'); // Loaded with utils/routes-loader.js
 const container = document.getElementById('container');
+ReactDOM.render(<Provider store={store}><Homepage /></Provider>, container);
 
-function renderComponent(component) {
-
-  ReactDOM.render(<Provider store={store}>{component}</Provider>, container);
-}
-
-// Find and render a web page matching the current URL path,
-// if such page is not found then render an error page (see routes.json, core/router.js)
-function render(location) {
-  router.resolve(routes, location)
-    .then(renderComponent)
-    .catch(error => console.error(error.stack));
-}
-
-// Handle client-side navigation by using HTML5 History API
-// For more information visit https://github.com/ReactJSTraining/history/tree/master/docs#readme
-history.listen(render);
-render(history.getCurrentLocation());
-
-// Eliminates the 300ms delay between a physical tap
-// and the firing of a click event on mobile browsers
-// https://github.com/ftlabs/fastclick
-FastClick.attach(document.body);
 
 // Enable Hot Module Replacement (HMR)
 if (module.hot) {
